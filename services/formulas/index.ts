9336796fd3bd147f402f261b1c35f88371104db9
7nani4u <7nani4u@naver.com>
2026-06-12 19:02:51 +0900

    Delete services directory

--- services/formulas/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './engine645';
-export * from './quantumFlux';
-export * from './neuralPattern';
-export * from './quantumAnalysis';
-export * from './advancedCluster';
-export * from './deepLearning3D';
-export * from './unifiedQuantumEngine';
